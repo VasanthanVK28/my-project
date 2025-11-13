1876da2fd3cf046f00d5ebdcba88280188c05091
Your Name <you@example.com>
2025-11-13 15:16:51 +0530

    Third party website Successfully Fininshed(13.11.2025)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import './App.css'
 function App() {
   return (
     <BrowserRouter>
